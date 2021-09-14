473df3ef3a53e4b6d8a23de89be95d6f51cd7ebe
yeo-seungwon <39931968+yeo-seungwon@users.noreply.github.com>
2021-09-14 14:55:08 +0900

    동전 0 문제 수정

---
+++ 2주차 과제/[백준] 동전 0.py
@@ -0,0 +1,17 @@
+# 가지고 있는 동전의 종류 n / 동전 금액의 합 k
+N, K = map(int,input().split())
+coin = []
+# 가지고 있는 동전의 종류 coin 리스트에 삽입
+for i in range(N):
+    coin.append(int(input()))
+coin.sort(reverse=True) # 내림차순 정렬
+
+count_c = 0
+# 사용되는 동전의 수 만큼 count
+for j in coin:
+    if K == 0:
+        break
+    count_c += K // j
+    K = K % j
+
+print(count_c)
