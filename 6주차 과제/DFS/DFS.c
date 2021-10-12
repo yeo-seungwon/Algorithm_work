79daafd4babec0d77cc36e2a5b71c2c80ef7d932
yeo-seungwon <39931968+yeo-seungwon@users.noreply.github.com>
2021-10-12 20:06:30 +0900

    DFS C, C++ 코드 수정

--- 6주차 과제/DFS/DFS.c
+++ 6주차 과제/DFS/DFS.c
@@ -1,25 +1,27 @@
-// #include <stdio.h>
-// #include <stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-// int dfs(int x);
+int dfs(int x);
 
-// bool visited[9];
-// int graph[9][3] = { {}, {2,3,8}, {1,7}, {1,4,5}, {3,5}, {3,4}, {7}, {2,6,8}, {1,7} };
+bool visited[9];
+int graph[9][3] = { {0}, {2,3,8}, {1,7}, {1,4,5}, {3,5}, {3,4}, {7}, {2,6,8}, {1,7} };
 
-// int main(void) {
-//     dfs(1);
-// }
+int main(void) {
+    dfs(1);
+}
 
-// int dfs(int x) {
-//     visited[x] = true;
-//     printf("%d ", x);
+int dfs(int x) {
+    visited[x] = true;
+    if (x != 0) {
+        printf("%d ", x);
+    }
 
-//     for (int i = 0; i < sizeof(graph[x]); i++) {
-//         int node = graph[x][i];
-//         if (!visited[node]) {
-//             dfs(node);
-//         }
-//     }
+    for (int i = 0; i < sizeof(graph[x]-1); i++) {
+        int node = graph[x][i];
+        if (!visited[node]) {
+            dfs(node);
+        }
+    }
 
-//     return  0;
-// }
+    return  0;
+}
--- 6주차 과제/DFS/DFS.c++
+++ 6주차 과제/DFS/DFS.c++
@@ -7,7 +7,9 @@ int graph[9][3] = {{}, {2,3,8}, {1,7}, {1,4,5}, {3,5}, {3,4}, {7}, {2,6,8}, {1,7
 
 void dfs(int x) {
     visited[x] = true; // 해당 번호의 노드 방문 처리
-    cout << x << ' '; // 해당 번호 출력 후 공백 출력
+    if(x != 0) {
+        cout << x << ' '; // 해당 번호 출력 후 공백 출력
+    }
     for (int node : graph[x]) {
     // 인접한 노드가 방문한 적이 없다면 DFS 수행
         if(!visited[node]) {
