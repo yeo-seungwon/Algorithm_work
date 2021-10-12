827e1c3a3c18f05a6db92d9b4deae10c28151a52
yeo-seungwon <39931968+yeo-seungwon@users.noreply.github.com>
2021-10-12 23:00:16 +0900

    BFS.c 코드 수정

---
+++ 6주차 과제/BFS/BFS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void bfs(int x);
+
+bool visited[9];
+int graph[9][3] = { {0}, {2,3,8}, {1,7}, {1,4,5}, {3,5}, {3,4}, {7}, {2,6,8}, {1,7} };
+
+int main(void) {
+    bfs(1);
+}
+
+void bfs(int x) {
+    
+
+    return  0;
+}
