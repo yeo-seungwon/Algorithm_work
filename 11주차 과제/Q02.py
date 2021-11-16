e20a1315be542e1327e944bbda7442ae7ac76cfa
yeo-seungwon <39931968+yeo-seungwon@users.noreply.github.com>
2021-11-16 17:35:45 +0900

    11주차 2번 문제

--- 11주차 과제/Q02.py
+++ 11주차 과제/Q02.py
@@ -1,4 +1,5 @@
 def findElement(arr, n):
+    answer = []
     leftMax = [None] * n
     leftMax[0] = arr[0]
  
@@ -13,12 +14,15 @@ def findElement(arr, n):
 
     for i in range(1, n-1):
         if leftMax[i-1] <= arr[i] and arr[i] <= rightMin[i+1]:
-            return i
+            answer.append(i)
  
-    return -1
+    if len(answer) == 0 :
+        return -1
+
+    return answer
  
 if __name__ == "__main__":
 
     arr = list(map(int, input().split(',')))
     n = len(arr)
-    print("해당 조건을 만족하는 인덱스 번호 : ", findElement(arr, n))
+    print("해당 조건을 만족하는 모든 인덱스 번호 : ", findElement(arr, n))
