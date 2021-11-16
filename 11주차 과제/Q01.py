251fe231e1515c7af8f44fb366335f9b795db6ff
yeo-seungwon <39931968+yeo-seungwon@users.noreply.github.com>
2021-11-16 17:29:59 +0900

    11주차 과제 1차 업로드

---
+++ 11주차 과제/Q01.py
@@ -0,0 +1,24 @@
+def findElement(arr, n):
+    leftMax = [None] * n
+    leftMax[0] = arr[0]
+ 
+    for i in range(1, n):
+        leftMax[i] = max(leftMax[i-1], arr[i-1])
+ 
+    rightMin = [None]*n
+    rightMin[n-1] = arr[n-1]
+ 
+    for i in range(n-2, -1, -1):
+        rightMin[i] = min(rightMin[i+1], arr[i])
+
+    for i in range(1, n-1):
+        if leftMax[i-1] <= arr[i] and arr[i] <= rightMin[i+1]:
+            return i
+ 
+    return -1
+ 
+if __name__ == "__main__":
+
+    arr = list(map(int, input().split(',')))
+    n = len(arr)
+    print("해당 조건을 만족하는 인덱스 번호 : ", findElement(arr, n))
